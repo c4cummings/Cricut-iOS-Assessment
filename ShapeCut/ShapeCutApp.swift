fe9a3e3c715d5bb74feaead91660e350f88bacff
Chad Cummings <c4cummings@icloud.com>
2024-08-28 00:09:22 -0600

    Initial Commit

---
+++ ShapeCut/ShapeCutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapeCutApp.swift
+//  ShapeCut
+//
+//  Created by Chad Cummings on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct ShapeCutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
